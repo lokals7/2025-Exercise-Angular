1ac4580e32b0324a38d2be7d40f85cff29b68c73
lokals7 <kentpaul777@gmail.com>
2025-07-10 16:05:15 +0800

    1st exercise - 7/10/2025

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -51,6 +52,17 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+  onAddTask(taskData: NewTaskData) {
+    const newTask = {
+      id: `t${this.tasks.length + 1}`, // Simple ID generation
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    };
+    this.tasks.push(newTask);
     this.isAddingTask = false;
   }
 }
